8e5e665fd520414b5fc7a15663e6f93af22c5d24
Robert Xiao <brx@cs.ubc.ca>
2019-05-11 01:00:51 -0700

    malloc_preload: Add realloc, calloc, posix_memalign

--- malloc_preload/malloc_preload.c
+++ malloc_preload/malloc_preload.c
@@ -5,8 +5,10 @@
 #include <stdio.h>
 #include <time.h>
 #include <fcntl.h>
-#define PRINT(x) write(1, x, strlen(x))
-#define MY_PRINTF(bufsz, fmt, ...) { char buf[bufsz]; sprintf(buf, fmt, ##__VA_ARGS__); PRINT(buf); }
+#include <errno.h>
+
+#define PRINT(x) write(2, x, strlen(x))
+#define MY_PRINTF(bufsz, fmt, ...) { char buf[bufsz]; sprintf(buf, "[pid %d] " fmt, getpid(), ##__VA_ARGS__); PRINT(buf); }
 
 void *malloc(size_t size) {
 	static void *(*fn)(size_t);
@@ -18,11 +20,56 @@ void *malloc(size_t size) {
 	return ptr;
 }
 
+static void *dlsym_calloc(size_t size, size_t size2) {
+	return NULL;
+}
+
+void *calloc(size_t size, size_t size2) {
+	static void *(*fn)(size_t, size_t);
+	if(!fn) {
+		/* dlsym calls calloc, so be careful here */
+		fn = dlsym_calloc;
+		fn = dlsym(RTLD_NEXT, "calloc");
+	}
+
+	void *ptr = fn(size, size2);
+
+	MY_PRINTF(256, "calloc(%zd, %zd) = %p\n", size, size2, ptr);
+	return ptr;
+}
+
+void *realloc(void *optr, size_t size) {
+	static void *(*fn)(void *, size_t);
+	if(!fn) fn = dlsym(RTLD_NEXT, "realloc");
+
+	void *ptr = fn(optr, size);
+
+	MY_PRINTF(256, "realloc(%p, %zd) = %p\n", optr, size, ptr);
+	return ptr;
+}
+
+int posix_memalign(void **memptr, size_t alignment, size_t size) {
+	static int (*fn)(void **, size_t, size_t);
+	if(!fn) {
+		fn = dlsym(RTLD_NEXT, "posix_memalign");
+	}
+
+	int res = fn(memptr, alignment, size);
+
+	if(res == 0) {
+		MY_PRINTF(256, "posix_memalign(%p, %zd, %zd) = %p\n", memptr, alignment, size, *memptr);
+	} else {
+		MY_PRINTF(256, "posix_memalign(%p, %zd, %zd) = error (%d)\n", memptr, alignment, size, errno);
+	}
+
+	return res;
+}
+
 void free(void *ptr) {
 	static void (*fn)(void *);
 	if(!fn) fn = dlsym(RTLD_NEXT, "free");
 
-	MY_PRINTF(32, "free(%p)\n", ptr);
+	MY_PRINTF(256, "free(%p) [0x%x bytes]\n", ptr, malloc_usable_size(ptr));
 
 	fn(ptr);
 }
