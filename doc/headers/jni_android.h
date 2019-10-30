53561f2fc61b8d5de272979ea169da59c6503e79
Robert Xiao <brx@cs.ubc.ca>
2019-10-30 09:30:11 -0700

    Add typedefs to jni_android.h to make Ghidra happier

--- doc/headers/jni_android.h
+++ doc/headers/jni_android.h
@@ -1,3 +1,9 @@
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+typedef unsigned short uint16_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+typedef signed long long int64_t;
 
 typedef uint8_t  jboolean; 
 typedef int8_t   jbyte;    
